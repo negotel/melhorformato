d6a0fd46bdb77ab563b8c90613691959db0097cb
negotel <edsonrcoosta@gmail.com>
2022-12-05 13:16:08 -0300

    Removendo arquivo .htaccess

--- source/Boot/Helpers.php
+++ source/Boot/Helpers.php
@@ -7,14 +7,6 @@
  *  @package Source\Boot 
  */
 
-function execInBackground($cmd)
-{
-    if (substr(php_uname(), 0, 7) == "Windows") {
-        pclose(popen("start /B " . $cmd, "r"));
-    } else {
-        exec($cmd . " > /dev/null &");
-    }
-}
 
 /**
  * ###############
